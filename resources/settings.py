fbddc8c879c11dcca30361cebd604e663cd461a4
KUNAL <you@example.com>
2021-05-03 17:16:26 +0530

    update

--- resources/settings.py
+++ resources/settings.py
@@ -163,13 +163,7 @@ CRONJOBS = [
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# CACHES_MIDDLEWARE_SECONDS = 60
-# CACHES = {
-#    'default': {
-#       'BACKEND': 'django_mongodb_cache.MongoDBCache',
-#       'LOCATION': 'mongodb+srv://iiitkalyani:mongodbkapassword@clusteRr0.hatca.mongodb.net/myFirstDatabase/my_cache?retryWrites=true&w=majority',
-#    }
-# }
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config['EMAIL_HOST_USER']
